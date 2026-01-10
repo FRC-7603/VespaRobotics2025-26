6714f252800732779b35d6c2488c282073bbde97
L1F3atbhss <Nathan.chan3000@gmail.com>
2026-01-10 16:15:59 -0500

    prep for pathauto and update to shooter

--- tank26/src/main/java/frc/robot/RobotContainer.java
+++ tank26/src/main/java/frc/robot/RobotContainer.java
@@ -7,14 +7,55 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
+import com.pathplanner.lib.auto.NamedCommands;
+
+// import com.pathplanner.lib.commands.PathPlannerAuto;
+// import com.pathplanner.lib.auto.NamedCommands;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Climber;
+
+// import com.ctre.phoenix.motorcontrol.ControlMode;
+// import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibj.Joystick;
+
+
 public class RobotContainer {
+
+  private final CommandXboxController joystickXBOX = new CommandXboxController(0);
+  private final Joystick stick = new Joystick(0);
+
+  public final Shooter shooter = new Shooter();
+  public final Climber climber = new Climber();
+
+
   public RobotContainer() {
+    NamedCommands.registerCommand("FuelIN", shooter.FuelInCommand());
     configureBindings();
   }
 
   private void configureBindings() {}
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+
+    // Optional<Alliance> al = DriverStation.getAlliance();
+    // if(al.isPresent()){
+    //     if(al.get() == Alliance.Blue){
+    //        return new PathPlannerAuto("Far Left Auto");
+    //     }
+    //     if(al.get() == Alliance.Red){
+    //         return new PathPlannerAuto("Far Left Auto RED");
+    //     }
+    // }
+    // return new PathPlannerAuto("Far Left Auto");
+    return null;
   }
 }
