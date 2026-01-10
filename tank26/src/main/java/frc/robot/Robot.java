1b51fcf117c7fc4786ab430595c28eb63ab75568
L1F3atbhss <Nathan.chan3000@gmail.com>
2026-01-10 14:12:10 -0500

    Rev,Climber,shooter and vision

--- tank26/src/main/java/frc/robot/Robot.java
+++ tank26/src/main/java/frc/robot/Robot.java
@@ -8,9 +8,36 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibj.Joystick;
 
+
+public class Robot extends TimedRobot {
+
+  // CAN IDs for drivetrain
+  private final VictorSPX motorL2 = new VictorSPX(1);
+  private final VictorSPX motorL1 = new VictorSPX(2);
+  
+  private final VictorSPX motorR2 = new VictorSPX(3);
+  private final VictorSPX motorR1 = new VictorSPX(4);
+  
+  // CAN IDs for shooter
+  private final VictorSPX bottomShooter = new VictorSPX(7);  
+  private final VictorSPX topShooter = new VictorSPX(10);
+  
+  // Joystick
+  private final Joystick stick = new Joystick(0);
+
+  // Shooter button mappings
+  private final int intakeButton = 5;
+  private final int shootButton  = 6;
+
+  // Shooter speeds
+  private final double intakespeed = 0.67;   // adjust as needed
+  private final double firingspeed = -1;  // negative if needed for outtake
+
+  private Command m_autonomousCommand;
   private final RobotContainer m_robotContainer;
 
   public Robot() {
@@ -54,7 +81,36 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    // Drivetrain Control 
+    double forward = stick.getRawAxis(0) / 3;   // left stick X
+    double rotation = -stick.getRawAxis(5) / 3;   // right stick Y
+
+    double leftSpeed = forward + rotation;
+    double rightSpeed = forward - rotation;
+
+    motorL1.set(ControlMode.PercentOutput, leftSpeed);
+    motorL2.set(ControlMode.PercentOutput, leftSpeed);
+    motorR1.set(ControlMode.PercentOutput, rightSpeed);
+    motorR2.set(ControlMode.PercentOutput, rightSpeed);
+
+    // Shooter Control 
+    if (stick.getRawButton(intakeButton)) {
+      // Intake: pull game piece in
+      bottomShooter.set(ControlMode.PercentOutput, intakespeed);
+      topShooter.set(ControlMode.PercentOutput, intakespeed);
+    } 
+    else if (stick.getRawButton(shootButton)) {
+      // Shoot/outtake
+      bottomShooter.set(ControlMode.PercentOutput, firingspeed);
+      topShooter.set(ControlMode.PercentOutput, firingspeed);
+    }
+  else {
+    // Stop shooters
+    bottomShooter.set(ControlMode.PercentOutput, 0);
+    topShooter.set(ControlMode.PercentOutput, 0);
+  }
+}
 
   @Override
   public void teleopExit() {}
