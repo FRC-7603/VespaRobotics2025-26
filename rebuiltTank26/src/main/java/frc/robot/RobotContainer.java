8eb6a0d6d357128863cd0f22ab54038bb6dbf509
yippii <liboris20@gmail.com>
2026-02-07 03:25:20 -0500

    Revert changes made that was overwritten

--- rebuiltTank26/src/main/java/frc/robot/RobotContainer.java
+++ rebuiltTank26/src/main/java/frc/robot/RobotContainer.java
@@ -10,8 +10,9 @@ import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
-import com.pathplanner.lib.commands.PathPlannerAuto;
-import com.pathplanner.lib.auto.NamedCommands;
+//idk if we can use pathplanner cuz no encoders
+//import com.pathplanner.lib.commands.PathPlannerAuto;
+//import com.pathplanner.lib.auto.NamedCommands;
 
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -29,13 +30,16 @@ import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.VisionSubsystem;
 import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.Intake;
+import frc.robot.Constants.ControllerConstants;
 import frc.robot.subsystems.CANDriveSubsystem;
 import frc.robot.subsystems.CANFuelSubsystem;
-//import frc.robot.subsystems.AutoLockAprilTag;
 
 public class RobotContainer {
 
   private final CommandXboxController joystickXBOX = new CommandXboxController(1);
+
+
+  // the only controller that is used right now
   private final Joystick stick = new Joystick(0);
 
   public final Shooter shooter = new Shooter();
@@ -44,6 +48,8 @@ public class RobotContainer {
   public final CANDriveSubsystem driveSubsystem = new CANDriveSubsystem();
   public final CANFuelSubsystem fuelSubsystem = new CANFuelSubsystem();
   public final VisionSubsystem visionSubsystem = new VisionSubsystem();
+
+  // Reserved for future reference if AprilTags would work
   //public final AutoLockAprilTag autoLockSubsystem = new AutoLockAprilTag(driveSubsystem, visionSubsystem);
 
   // // The operator's controller
