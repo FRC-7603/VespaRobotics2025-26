54b20ee43df5a6eea84d4d12071326834202fe77
yippii <liboris20@gmail.com>
2026-02-26 16:11:17 -0500

    Added current limit subclass
    Constants file created current limit subclass, updated imports for affected subsystems

--- rebuiltTank26/src/main/java/frc/robot/subsystems/CANFuelSubsystem.java
+++ rebuiltTank26/src/main/java/frc/robot/subsystems/CANFuelSubsystem.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.*;
+import static frc.robot.Constants.MotorCurrentLimits.*;
 import static frc.robot.Constants.FuelConstants.*;
 
 public class CANFuelSubsystem extends SubsystemBase {
