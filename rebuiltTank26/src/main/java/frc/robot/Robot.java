91f571b8e9a34cdc9ab45f053fadc9b9f33fb2e4
yippii <liboris20@gmail.com>
2026-03-13 10:13:24 -0400

    Added SmartDashboard initial support and a chooser for auto

--- rebuiltTank26/src/main/java/frc/robot/Robot.java
+++ rebuiltTank26/src/main/java/frc/robot/Robot.java
@@ -7,16 +7,26 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.RobotContainer;
-
+import frc.robot.subsystems.BackupAuto;
 
 public class Robot extends TimedRobot {
 
   private Command m_autonomousCommand;
-  private final RobotContainer m_robotContainer;
+  private static final String kDefaultAuto = "Default";
+  private static final String kBackupAuto = "Backup Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+  private final RobotContainer m_robotContainer = new RobotContainer();
+  private final BackupAuto m_backupAuto = new BackupAuto();
 
   public Robot() {
-    m_robotContainer = new RobotContainer();
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("Backup Auto", kBackupAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
   }
 
   @Override
@@ -35,15 +45,25 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
+    m_autoSelected = m_chooser.getSelected();
+    System.out.println("Auto selected: " + m_autoSelected);
   }
 
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+    switch(m_autoSelected) {
+      case kBackupAuto:
+        m_autonomousCommand = m_backupAuto.simpleAuto();
+        break;
+      case kDefaultAuto:
+      default:
+        m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+        if (m_autonomousCommand != null) {
+          m_autonomousCommand.schedule();
+        }
+        break;
+    }
+  }
 
   @Override
   public void autonomousExit() {}
