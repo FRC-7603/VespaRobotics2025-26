2288a30b9adf912b88ff05d23a0c33e8e8b09dd2
L1F3atbhss <Nathan.chan3000@gmail.com>
2026-03-12 20:24:03 -0400

    backup auto for comp
    WE ARE SO COOKED

--- rebuiltTank26/src/main/java/frc/robot/RobotContainer.java
+++ rebuiltTank26/src/main/java/frc/robot/RobotContainer.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj2.command.Commands;
 // import com.pathplanner.lib.auto.NamedCommands;
 // import edu.wpi.first.wpilibj2.command.CommandScheduler;
 // import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
-
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 // DS
 // import edu.wpi.first.wpilibj2.command.button.Trigger;
 // import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -116,5 +116,55 @@ public class RobotContainer {
 
       return m_visionSubsystem.driveToTagLEBRON(m_driveSubsystem, 1.5);
 
+      //return simpleAuto();
+
   }
+
+
+public Command simpleAuto() {
+    return Commands.sequence(
+
+        // Step 1: step back 1.5 meters (approx)
+        Commands.run(() -> m_driveSubsystem.driveArcade(-0.5, 0), m_driveSubsystem).withTimeout(2),
+        m_driveSubsystem.driveStopCommand(),
+
+        // Step 2: shoot
+        m_fuel.shootingCommand(),
+        new WaitCommand(1.5),
+
+        // Step 3: step back
+        Commands.run(() -> m_driveSubsystem.driveArcade(-0.5, 0), m_driveSubsystem).withTimeout(1),
+        m_driveSubsystem.driveStopCommand(),
+
+        // Step 4: turn 30°
+        Commands.run(() -> m_driveSubsystem.driveArcade(0, 0.4), m_driveSubsystem).withTimeout(0.5),
+        m_driveSubsystem.driveStopCommand(),
+
+        // Step 5: go
+        Commands.run(() -> m_driveSubsystem.driveArcade(0.6, 0), m_driveSubsystem).withTimeout(2),
+        m_driveSubsystem.driveStopCommand(),
+
+        // Step 6: turn
+        Commands.run(() -> m_driveSubsystem.driveArcade(0, -0.4), m_driveSubsystem).withTimeout(0.7),
+        m_driveSubsystem.driveStopCommand(),
+
+        // Step 7: go
+        Commands.run(() -> m_driveSubsystem.driveArcade(0.6, 0), m_driveSubsystem).withTimeout(2),
+        m_driveSubsystem.driveStopCommand(),
+
+        // Step 8: intake
+        m_fuel.groundIntakeCommand(),
+        new WaitCommand(2),
+        m_fuel.stopCommand(),
+
+        // Step 9: turn 180
+        Commands.run(() -> m_driveSubsystem.driveArcade(0, 0.6), m_driveSubsystem).withTimeout(2),
+        m_driveSubsystem.driveStopCommand(),
+
+        // Step 10: go
+        Commands.run(() -> m_driveSubsystem.driveArcade(0.6, 0), m_driveSubsystem).withTimeout(2),
+        m_driveSubsystem.driveStopCommand()
+    );
+}
+
 }
